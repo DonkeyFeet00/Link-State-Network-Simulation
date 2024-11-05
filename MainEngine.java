8a8fd8883959501365810d38ec6c3e94045b66dd
DonkeyFeet00 <76487056+DonkeyFeet00@users.noreply.github.com>
2024-11-04 18:32:06 -0600

    add code supplied by assignment page

--- MainEngine.java
+++ MainEngine.java
@@ -1,2 +1,11 @@
 public class MainEngine {
+    //array of routers
+    public static void main(String[] args) {
+//        step1: Run the initializer
+//        step2: Let the routers run their builder and listener functions,
+//                and then display the developed routing table per router.
+//        step3: Pick a random router(s), and call change_cost(),
+//                then display the developed routing table to make sure that the updates are taking place.
+    }
+
 }
