63b41893c180fcf37fcfb46f99222be9d2a47132
DonkeyFeet00 <76487056+DonkeyFeet00@users.noreply.github.com>
2024-11-04 18:31:35 -0600

    add variables and methods

--- Router.java
+++ Router.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+
 public class Router {
+    private String name;
+    private ArrayList<String> neighbors = new ArrayList<String>();
+    private ArrayList<String> cost = new ArrayList<String>();
+    private ArrayList<String> destinations = new ArrayList<String>();
+    private ArrayList<String> routingTable = new ArrayList<String>();
+
+
+    public Router(/*TODO*/) {
+        /*TODO*/
+    }
+
+    public void displayTable(/*TODO*/) {
+        /*TODO*/
+    }
+
+    public void builder(/*TODO*/) {
+        /*TODO*/
+    }
+
+    public void listener(/*TODO*/) {
+        /*TODO*/
+    }
+
+    public void changeCost(/*TODO*/) {
+        /*TODO*/
+    }
 }
