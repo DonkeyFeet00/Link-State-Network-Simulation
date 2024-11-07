2201a8b109f64a03dfb63fae3f580f7e79ef45c9
DonkeyFeet00 <76487056+DonkeyFeet00@users.noreply.github.com>
2024-11-07 12:08:00 -0600

    add template and file scanner

--- Initializer.java
+++ Initializer.java
@@ -1,2 +1,18 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Initializer {
+/*
+    input file has the following format:
+    Number of routers
+    Router label: (neighbor label, cost); (neighbor label, cost); ….
+    Router label: (neighbor label, cost); (neighbor label, cost); ….
+            …..
+    Router label: (neighbor label, cost); (neighbor label, cost); ….
+ */
+
+    File inputFile = new File("Input");
+    Scanner fileScanner = new Scanner(inputFile);
+
+
 }
