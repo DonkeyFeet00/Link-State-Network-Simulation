049cdde11df6715b923b6611efe62590dcf464b3
GlacialLight <aertew12@gmail.com>
2024-11-19 15:39:14 -0600

    test

--- MainEngine.java
+++ MainEngine.java
@@ -2,6 +2,8 @@ public class MainEngine {
     //array of routers
     public static void main(String[] args) {
 //        step1: Run the initializer
+Initializer init=new Initlizer();
+init.readFile();
 //        step2: Let the routers run their builder and listener functions,
 //                and then display the developed routing table per router.
 //        step3: Pick a random router(s), and call change_cost(),
