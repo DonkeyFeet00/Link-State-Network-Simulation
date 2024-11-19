8f37ed983c4c51082af7ccf94a035050d8f573f2
DonkeyFeet00 <76487056+DonkeyFeet00@users.noreply.github.com>
2024-11-19 16:22:13 -0600

    moved line

--- src/MainEngine.java
+++ src/MainEngine.java
@@ -1,10 +1,12 @@
 public class MainEngine {
     //array of routers
     public static void main(String[] args) {
+        Initializer init = new Initializer();
+        init.readFile("Input");
+
 
 //        step1: Run the initializer
-Initializer init = new Initlizer();
-init.readFile();
+
 //        step2: Let the routers run their builder and listener functions,
 //                and then display the developed routing table per router.
 //        step3: Pick a random router(s), and call change_cost(),
