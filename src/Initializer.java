88a811eb98c811664da35f14ba6bfdc8dd024320
DonkeyFeet00 <76487056+DonkeyFeet00@users.noreply.github.com>
2024-11-20 08:46:11 -0600

    add partial code for loop to create routers

--- src/Initializer.java
+++ src/Initializer.java
@@ -15,11 +15,21 @@ public class Initializer {
         try {
             File inFile = new File(fileName);
             Scanner fileScanner = new Scanner(inFile);
+
+            //grab number on the first line of the file as our number of routers
+            int numRouters = fileScanner.nextInt();
+
+            //loop through all routers in the file
+            while(fileScanner.hasNextLine()) {
+                String currentLine = fileScanner.nextLine();
+                String currentName = currentLine.substring(0, currentLine.indexOf(':'));
+
+
+
+            }
         }
         catch(Exception e) {
             System.out.println("Error opening file: " + e);
         }
     }
-
-
 }
